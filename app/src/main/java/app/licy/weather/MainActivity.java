e1896c964a046e92040c581c9384f0a700582380
GitHub-Lichengyang <licy3051@qq.com>
2019-04-02 21:14:09 +0800

    first commit

--- app/src/main/java/app/licy/weather/MainActivity.java
+++ app/src/main/java/app/licy/weather/MainActivity.java
@@ -3,11 +3,22 @@ package app.licy.weather;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * MainActivity
+ * description: 程序主页面
+ *
+ * @author : Licy
+ * @date : 2019年3月25日
+ * email ：licy3051@qq.com
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
     }
 }
